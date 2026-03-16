2447b55df86fb6c8cd2c9b8c37e6e739f193d354
Aleksander Polanski <s33173@pjwstk.edu.pl>
2026-03-16 14:52:03 +0100

    Fixed iris[][] problem

--- MPP1/Program.cs
+++ MPP1/Program.cs
@@ -1,20 +1,52 @@
-﻿namespace MPP1;
+﻿using System.Globalization;
+
+namespace MPP1;
 
 class Program
 {
     static void Main(string[] args)
     {
         string text = File.ReadAllText("iris.txt");
-        Console.WriteLine(text);
+        
+        string[] lines = text.Split('\n');
+        double[][] iris_vals = new double[lines.Length][];
+        
+
+        for (int i = 1; i < lines.Length; i++)
+        {
+            string[] tempArr= lines[i].Split(',');
+            
+
+            iris_vals[i] = new double[4];
+            for (int j = 0; j < 4; j++)
+            {
+                iris_vals[i][j] = double.Parse(tempArr[j], CultureInfo.InvariantCulture);
+                Console.WriteLine(iris_vals[i][j]);
+            }
+            
+        }
     }
 }
 
-class Calssifier
+public static class Calssifier
 {
-    private static double[][] iris;
+    private static List<int>[] iris_values;
+
+
+
+    public static void FormatString(string text)
+    {
+        string[] lines = text.Split('\n');
+
+        for (int i = 1; i < lines.Length; i++)
+        {
+            double[] tempArr= lines[i].Split(',').Select(double.Parse).ToArray();
+            Console.WriteLine(tempArr[i]);
+        }
+    }
     
     
-    public double Distance(double[] a,  double[] b)
+    public static double Distance(double[] a,  double[] b)
     {
         if (a.Length == b.Length)
         {
